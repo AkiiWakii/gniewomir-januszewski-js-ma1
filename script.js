89593fa75dba30e25fc02c372ebcc1860a3b8757
Gneffi <37945505+AkiiWakii@users.noreply.github.com>
2020-03-29 22:26:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// Question 1
+let cat = {complain : meow};
+// Question 2
+document.querySelector("h3");
+//Question 3
+const heading3 = document.querySelector("h3");
+heading3.style.fontSize = "2em";
+//Question 4
+ heading3.classList.add("subheading");
+//Question 5 
+const paragraphs = document.querySelectorAll("p");
+//Question 6
+const resultsContainer = document.getElementsByClassName("results");
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+//Question 7
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+function logName(catArray){
+    let catName = cats.name;
+    for(let i = 0; i < cats.length; i++);
+    console.log(catArray[i])
+};
+//Question 8
+
+//Question 9 
+
+//Question 10
